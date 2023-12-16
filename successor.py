d5c82d31129b8c4cacab364f9826f3a5ea3de40c
mahdirad82 <150522040+mahdirad82@users.noreply.github.com>
2023-12-16 11:34:59 +0330

    Update successor.py

--- successor.py
+++ successor.py
@@ -1,4 +1,34 @@
-matrix = []
+from main import matrix
+
+
+def numerisingMatrix(matrix):
+    targets = []
+    initial_state = None
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if "R" in matrix[i][j]:
+                matrix[i][j] = matrix[i][j].replace("R", "")
+                initial_state = (i, j)
+            elif "T" in matrix[i][j]:
+                matrix[i][j] = matrix[i][j].replace("T", "")
+                targets.append((i, j))
+            if matrix[i][j].isdigit():
+                matrix[i][j] = int(matrix[i][j])
+
+            elif "C" in matrix[i][j]:
+                matrix[i][j] = matrix[i][j].replace("C", "")
+                matrix[i][j] = int(matrix[i][j]) - 10
+            elif "B" in matrix[i][j]:
+                matrix[i][j] = matrix[i][j].replace("B", "")
+                matrix[i][j] = int(matrix[i][j]) - 5
+            elif "I" in matrix[i][j]:
+                matrix[i][j] = matrix[i][j].replace("I", "")
+                matrix[i][j] = int(matrix[i][j]) - 12
+
+    return [matrix, initial_state, targets]
+
+
+standardMatrix, initial_state, targets = numerisingMatrix(matrix)
 
 
 def get_successors(curr_pos):
